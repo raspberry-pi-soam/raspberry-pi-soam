64e7f562b1d750fbbf0b8cfe1b507ecd87b63f06
raspberry-pi-soam <ubuntu20619@gmail.com>
2025-10-07 11:09:55 +0530

    Added 7 Segment Display Files

---
+++ RPiPracs/4digittime/clock.py
@@ -0,0 +1,19 @@
+import sys
+import time
+import datetime
+import RPi.GPIO as GPIO
+import tm1637
+#CLK -> GPIO23 (Pin 16)
+#Di0 -> GPIO24 (Pin 18)
+Display = tm1637.TM1637(23,24,tm1637.BRIGHT_TYPICAL)
+Display.clear()
+Display.brightness(1)
+while(True):
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    currenttime = 1234
+    currenttime = f"{hour:02d}{minute:02d}"  # currenttime = "1305"
+    Display.show(currenttime, colon=(second % 2 == 1))
+    time.sleep(1)
