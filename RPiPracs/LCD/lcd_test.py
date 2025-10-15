6cf4bc9948c67a94575984d8f77378233f55e114
raspberry-pi-soam <ubuntu20619@gmail.com>
2025-10-15 21:17:21 +0530

    Update lcd_test.py

--- RPiPracs/LCD/lcd_test.py
+++ RPiPracs/LCD/lcd_test.py
@@ -1,6 +1,14 @@
 from RPLCD.i2c import CharLCD
 from time import sleep
-
+# gnd - 6
+# vcc - 4
+# sda - 3
+# scl - 5
+# sudo raspi-config > I2C
+# sudo reboot and update, upgrade apt
+# sudo apt install i2c-tools python3-smbus python3-pip
+# sudo pip3 install RPLCD
+# i2cdetect -y 1
 
 lcd = CharLCD('PCF8574', 0x27)  # Change 0x27 if needed
 
