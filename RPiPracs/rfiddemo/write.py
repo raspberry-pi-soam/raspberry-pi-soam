a7c4022b208997935b8499d8a4314a32389830be
raspberry-pi-soam <ubuntu20619@gmail.com>
2025-10-16 08:01:52 +0530

    Update write.py

--- RPiPracs/rfiddemo/write.py
+++ RPiPracs/rfiddemo/write.py
@@ -1,19 +1,6 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-# SDA - 24
-# SCK - 23
-# MOSI - 19
-# MISO - 21 
-# GND - 6 , VCC / 3.3V - 1
-# RST - 22
-# sudo raspi-config > SPI
-# apt-get update & upgrade
-# sudo apt-get install python3-dev python3-pip
-# sudo pip3 install spidev
-# sudo pip3 install mfrc522
-# run write.py then read.py
-#welcome message
 print("Looking for cards")
 print("Press Ctrl+c to STOP")
 try:
@@ -23,3 +10,29 @@ try:
     print("Data Written Successfully	")
 finally:
     GPIO.cleanup()
+
+
+
+
+
+
+
+
+
+
+
+    
+# SDA - 24
+# SCK - 23
+# MOSI - 19
+# MISO - 21 
+# GND - 6 , VCC / 3.3V - 1
+# RST - 22
+# apt-get update & upgrade
+# sudo raspi-config > SPI
+# sudo reboot 
+# sudo apt-get install python3-dev python3-pip
+# sudo pip3 install spidev
+# sudo pip3 install mfrc522
+# run write.py then read.py
+#welcome message
